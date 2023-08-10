054f9e0b0c1f0da06e20c1dbef31edd8de5d4454
NIDfrontend <nidfrontend@gmail.com>
2023-08-10 18:36:41 +0700

    Second slider is ready!

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.hotel-slider', {
+const hotelSlider = new Swiper('.hotel-slider', {
 
   loop: true,
 
@@ -6,5 +6,17 @@ const swiper = new Swiper('.hotel-slider', {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
- 
+  effect: "coverflow",
+});
+
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+
+  loop: true,
+
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  
 });
