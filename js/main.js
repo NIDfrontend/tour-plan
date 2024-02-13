334853d2404f6ae99fc12d5e4211401d6ed025c9
NIDfrontend <nidfrontend@gmail.com>
2024-02-13 16:39:28 +0700

    Media: mobile menu

--- js/main.js
+++ js/main.js
@@ -20,3 +20,10 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   },
   
 });
+
+var menuButton = document.querySelector (".menu-button");
+menuButton.addEventListener('click', function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom")
+  .classList.toggle('navbar-bottom_visible');
+});
